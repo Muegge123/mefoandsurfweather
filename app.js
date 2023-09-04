bdeb050184edf6688a522cd8877479a8404ed34d
Dennis <dennis.redepenning@gmail.com>
2023-09-04 10:52:52 +0200

    determined data and basic layout

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+// DWD API
+// https://s3.eu-central-1.amazonaws.com/app-prod-static.warnwetter.de/v16/forecast_mosmix_10184.json
+
 function test() {
   alert("test");
+}
+
+let url =
+  "https://s3.eu-central-1.amazonaws.com/app-prod-static.warnwetter.de/v16/forecast_mosmix_10184.json";
+
+async function getData() {
+  let res = await fetch(url);
+  let data = await res.json();
+  let windDegree = data.forecast.windDirection[2];
+  console.log(windDegree.toString().slice(0, 3));
+
+  renderTableBody(data);
+}
+
+function renderTableBody(data) {
+  document.getElementById("wind-degree").innerHTML =
+    data.forecast.windDirection[2].toString().slice(0, 3);
+}
+
+function getPokemonTest() {
+  console.log(spotDescriptions.spots[0].name);
 }
---
+++ spotdescriptiondata.js
@@ -0,0 +1,24 @@
+const spotDescriptions = {
+  spots: [
+    {
+      id: 0,
+      name: "Brodtner Ufer",
+      dwdStationNr: 12345,
+      mapUrl: "./img/brodtner-ufer-test.png",
+      perfectWinddirection: ["NW", "W", "SO"],
+      okWinddirection: ["N", "S"],
+      waveHeightMax: 0.5,
+      waterLevel: ["normal", "hoch"],
+    },
+    {
+      id: 1,
+      name: "Bliesdorf",
+      dwdStationNr: 23456,
+      mapUrl: "./img/brodtner-ufer-test.png",
+      perfectWinddirection: ["NW", "W", "SO"],
+      okWinddirection: ["N", "S"],
+      waveHeightMax: 0.5,
+      waterLevel: ["normal", "hoch"],
+    },
+  ],
+};
